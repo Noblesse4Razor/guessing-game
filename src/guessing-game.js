58fb6f7a4685a76c39503e877fada5f019b17d41
Noblesse4Razor <noblesse4razor@gmail.com>
2017-01-28 03:32:41 +0300

    to lowerfloating

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		
+	}
 
     setRange(min, max) {
-
+	this.Range = new Array();
+	for(let i=0;i<=max-min;i++)
+	{
+		this.Range[i]=i+min;
+	}
+	this.length = max - min; // not sure about my right to access default props of the Array class
     }
 
     guess() {
-
+	return this.Range[this.length>>1];
     }
 
     lower() {
-
+	this.length=this.length>>1;	
+	return this;
     }
 
     greater() {
-
+	this.setRange(this.Range[this.length>>1],this.Range[this.length])
+	return this;
     }
 }
 
